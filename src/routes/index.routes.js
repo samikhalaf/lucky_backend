1593fc8518dd9c7adc55335651ac8a9cab265b7d
Sami Khalaf <samifalafel>
2020-11-09 09:04:22 +0100

    GET, POST and DELETE working fine in pets

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 
-const petsRouter = require('./pets.routes')
-const usersRouter = require('./users.routes')
+const petsRouter = require('./pet.routes');
+const usersRouter = require('./user.routes');
+const testRouter = require('./test.routes');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-          res.status(200).send('Server running OK');
-          console.log(req)
- });
+  res.status(200).send('Server running OK');
+  console.log(req);
+});
 
-router.use('/pets', petsRouter)
-
-router.use('/users', usersRouter)
+router.use('/pets', petsRouter);
+router.use('/users', usersRouter);
+router.use('/test', testRouter);
 
 module.exports = router;
